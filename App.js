753dcde1a50429a0245ba52171c4f063b75ef96d
Madhu061095 <126377062+Madhu061095@users.noreply.github.com>
2023-02-28 01:12:55 +0530

    updated checkbox

--- App.js
+++ App.js
@@ -8,35 +8,25 @@ function App() {
   const [hide, showHide] = useState(false);
   // const { Panel } = Collapse;
   const [searchInput, setSearchInput] = useState("");
-  const providerArr = require("./Formatted.json");
-  // console.log(providerArr);
-  // providerArr.map((provider,index)=>{
-  //   providerArr[index].isChecked = false
-  // })
+  const [checkedArr, setCheckedArr] = useState({})
+  let providerArr = require("./Formatted.json");
+  providerArr.map((provider,index)=>{
+    providerArr[index].id = index+1;
+    providerArr[index].isChecked = false
+  })
+  console.log(providerArr)
   const [filterList, setFilterList] = useState(providerArr);
   const handleChange = (e) => {
     e.preventDefault();
-    // console.log(e.target.value);
     setSearchInput(e.target.value);
   };
 
-  // const handleChecked = (e) =>{
-  //   console.log(e.target.checked);
-  //   e.preventDefault();
-  //   // console.log(e.target.value);
-  //   providerArr.map((provider, index) =>{
-
-  //     if(provider.id == e.target.id){
-  //       console.log(index);
-  //       providerArr[index].isChecked = e.target.checked;
-  //     }
-  //   })
-  //   console.log(providerArr);
-  // }
+  const handleChecked = (e) =>{
+    setCheckedArr({...checkedArr,[e.target.id]: e.target.checked})
+  }
 
   useEffect(() => {
     if (searchInput.length > 0) {
-      // console.log(searchInput);
       setFilterList(
         providerArr.filter((providerName) => {
           return providerName.providerName
@@ -44,9 +34,8 @@ function App() {
             .match(searchInput.toUpperCase());
         })
       );
-      // console.log(test);
     }
-    if (searchInput.length == 0) {
+    if (searchInput.length === 0) {
       setFilterList(providerArr);
     }
   }, [searchInput]);
@@ -54,12 +43,8 @@ function App() {
   return (
     <div className="App">
       <div className="dropdown">
-        {/* <button class="btn btn-lg" style="background-color:transparent;">
-        <i class="fa fa-pencil"></i> Edit
-      </button> */}
-        {/* <button type="button" class="btn btn-light">Light</button> */}
         <Button onClick={() => showHide(!hide)} variant="light">
-        <i class="bi bi-sliders2"></i> Filter
+        <i className="bi bi-sliders2"></i> Filter
         </Button>
         {hide && (
           <div id="myDropdown" className="dropdown-content">
@@ -80,7 +65,7 @@ function App() {
                       style={{ backgroundColor: "none" }}
                       id="basic-addon1"
                     >
-                      <i class="bi bi-search"></i>
+                      <i className="bi bi-search"></i>
                     </InputGroup.Text>
                     <Form.Control
                       placeholder="search"
@@ -97,9 +82,9 @@ function App() {
                         id={provider.id}
                         type="checkbox"
                         name={provider.id}
-                        // checked={provider.isChecked}
-                        // onChange={handleChecked}
-                        // value={provider.providerName}
+                        checked={checkedArr[provider.id]}
+                        onChange={handleChecked}
+                        value={provider.isChecked}
                       />
                     ))}
                   </Form>
@@ -112,41 +97,6 @@ function App() {
                 </Accordion.Body>
               </Accordion.Item>
             </Accordion>
-            {/* <Row style={{ height: "35px" }} justify="space-around">
-            <Col style={{ margin: "auto" }} span={8}>Filter</Col>
-            <Col style={{ margin: "auto" }} span={8} offset={8}>
-              clear filter
-            </Col>
-          </Row> */}
-            {/* <Collapse >
-            <Panel header="Competitor" key="1">
-              
-              <div>
-
-                <input type="text" placeholder="Search here" onChange={handleChange} value={searchInput} />
-
-                <table>
-                  <>
-                   <Checkbox.Group style={{ width: '100%' }}>
-                    <Row className="checkboxDiv" >
-                      {filterList.map((provider) =>
-                        //  <Col span={24}>
-                                <Checkbox value={provider.providerName}>{provider.providerName}</Checkbox>
-                              // </Col>
-                      )}
-                      </Row>
-                      </Checkbox.Group>
-                  </>
-                </table>
-
-              </div>
-            </Panel>
-            <Panel header="Location" key="2">
-              <p>Location</p>
-              <p>Location</p>
-              <p>Location</p>
-            </Panel>
-          </Collapse> */}
           </div>
         )}
       </div>
